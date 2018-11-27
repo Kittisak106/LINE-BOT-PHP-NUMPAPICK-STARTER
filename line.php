d34ff56968815911bdf7d1bcf8de83873d1343f5
Kittisak106 <43182617+Kittisak106@users.noreply.github.com>
2018-11-28 01:51:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Ue38ab2c1c6c699ad1c8174ba463b5b8e',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
