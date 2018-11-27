7bf44b417198c53e6d728b49d3e6508eb997bd27
Kittisak106 <43182617+Kittisak106@users.noreply.github.com>
2018-11-28 00:43:46 +0700

    1

--- bot.php
+++ bot.php
@@ -27,14 +27,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 
-			$Topic = "NodeMCU1" ;
+			$Topic = "Unbalance" ;
 			getMqttfromlineMsg($Topic,$text);
 			   
 			
 		}
 	}
 }
-$Topic = "NodeMCU1" ;
+$Topic = "Unbalance" ;
 $text = "Test";
 getMqttfromlineMsg($Topic,$text);
 echo "OK3";
